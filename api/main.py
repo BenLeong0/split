90073ba46ea04fc3c4578ce4909006463f507e35
cosmina3 <cosmina@myurbanjungle.com>
2022-09-17 15:20:41 +0100

    first route

--- api/main.py
+++ api/main.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def ping():
+    return Response(201)
+
+
+# USERS
+
+# TODO
+@app.route("/api/create_user", methods=["OPTIONS", "POST"])
+def create_user():
+    if request.method == "OPTIONS":
+        return Response(201)
+    print('creating user...')
+    return Response(201)
