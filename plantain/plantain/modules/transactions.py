1eafb226a7e0e0e4f04d8ebbdc3a78f6a9037c71
samwinder <sam@myurbanjungle.com>
2022-09-17 18:25:04 +0100

    comment

--- plantain/plantain/modules/transactions.py
+++ plantain/plantain/modules/transactions.py
@@ -17,7 +17,6 @@ def store_transaction(store_transaction_request: StoreTransactionMessageData):
     transaction_components = store_transaction_request['components']
 
     with DatabaseConnection() as db:
-        # TODO: generate ID
         db.cursor.execute(
             f"INSERT INTO transactions"
             f"VALUES ({transaction_id} {date} {description})"
