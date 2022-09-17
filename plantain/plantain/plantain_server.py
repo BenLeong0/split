744205636205c41c4e6f761b2fc418ed6850c8a1
samwinder <sam@myurbanjungle.com>
2022-09-17 18:27:42 +0100

    corrected server

--- plantain/plantain/plantain_server.py
+++ plantain/plantain/plantain_server.py
@@ -4,7 +4,10 @@ from typing import Optional
 
 import pika
 
-import modules.users as users
+from modules import (
+    users,
+    transactions,
+)
 
 from split_types.message_types import Message
 
@@ -25,7 +28,7 @@ def consume_and_respond(_ch, _method, _properties, body) -> Optional[str]:
             store_transaction_request = message[1]
             return json.dumps((
                 "store_user_response",
-                users.store_user(store_user_request)
+                transactions.store_transaction(store_transaction_request)
             ))
 
     except Exception as e:
