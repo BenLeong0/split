1761487b061dea9e6bdb2db5083d66e9ecb8ae0b
samwinder <sam@myurbanjungle.com>
2022-09-17 17:14:04 +0100

    Added actual sql

--- plantain/plantain/modules/users.py
+++ plantain/plantain/modules/users.py
@@ -7,6 +7,14 @@ Split.
 from db import DatabaseConnection
 
 def store_user(store_user_request):
+    first_name = store_user_request['first_name']
+    last_name = store_user_request['last_name']
+    email = store_user_request['email']
+    password = store_user_request['password']
     with DatabaseConnection() as db:
-        db.cursor.execute("SELECT * FROM users")
+        # TODO: generate ID
+        db.cursor.execute(
+            f"INSERT INTO users"
+            f"VALUES ({1, first_name, last_name, email, password})"
+        )
         print(db.cursor.fetchall())
