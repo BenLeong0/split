3339621c8330211eee66c47e8befcb077c00e39e
benleong0 <benleong@myurbanjungle.com>
2022-09-30 22:27:23 +0100

    subrouter

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+import adminRouter from "./modules/admin";
 import userRouter from "./modules/user";
 
 dotenv.config();
@@ -8,6 +9,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 
 app.get("/", (req: Request, res: Response) => {
