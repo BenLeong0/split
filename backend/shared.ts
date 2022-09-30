9279f413a2133f039c2219451a8cbdf783ea5993
benleong0 <benleong@myurbanjungle.com>
2022-10-01 00:58:42 +0100

    check for if active in authentication

--- backend/shared.ts
+++ backend/shared.ts
@@ -1,19 +1,27 @@
 import { Request } from "express";
+import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
 
 import { AccessTokenData } from "./types";
+
+const prisma = new PrismaClient();
+
+// SHARED
 
 export const authenticate = async (req: Request) => {
   const { authorization: authHeader } = req.headers;
 
   const accessToken = authHeader?.split(" ")[1];
-  if (typeof accessToken === "undefined") throw Error();
+  if (typeof accessToken === "undefined") throw Error("invalid token");
 
   const decoded: unknown = jwt.verify(
     accessToken,
     process.env.ACCESS_TOKEN_SECRET
   );
-  return (decoded as AccessTokenData).userId;
+  const { userId } = decoded as AccessTokenData;
+
+  if (!(await isUserActive(userId))) throw Error("user is inactive");
+  return userId;
 };
 
 export const generateSuccessfulResponse = (data: any) => ({
@@ -25,3 +33,10 @@ export const generateErrorResponse = (errorMsg: string) => ({
   status: "error",
   error: errorMsg,
 });
+
+// HELPERS
+
+const isUserActive = async (userId: string) => {
+  const user = await prisma.user.findFirst({ where: { id: userId } });
+  return user != null && user.isActive;
+};
