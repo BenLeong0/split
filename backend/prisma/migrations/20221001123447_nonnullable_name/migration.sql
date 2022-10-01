bb2595ca6a03108fbb291b3e8c4d980088eee828
benleong0 <benleong@myurbanjungle.com>
2022-10-01 13:35:37 +0100

    non nullable user name

---
+++ backend/prisma/migrations/20221001123447_nonnullable_name/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "icon" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_User" ("createdAt", "email", "icon", "id", "isActive", "name", "updatedAt") SELECT "createdAt", "email", "icon", "id", "isActive", "name", "updatedAt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE INDEX "User_id_email_idx" ON "User"("id", "email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
